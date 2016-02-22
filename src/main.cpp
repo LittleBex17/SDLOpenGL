3b54b0b05a842166d89316213fa75c3d02529e01
LittleBex17 <i7678312@bournemouth.ac.uk>
2016-02-22 11:56:13 +0000

    Add a read me file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
-include <iostream>
+#include <iostream>
+#include "SDLOpenGL.h"
+#include <ngl/NGLInit.h>
+
+void initializedGL();
+void renderGL();
 
 int main(int argc, char *argv[])
 {
-    st::cout<<"working\n";
+    SDLOpenGL window("test",0,0,1024,720);
+    window.makeCurrent();
+    initializedGL();
+    while(true)
+    {
+        renderGL();
+        window.swapWindow();
+        SDL_Delay(10);
+    }
+}
+
+void initializedGL()
+{
+    ngl::NGLInit::instance();
+    glClearColor(0.8,0.8,0.8,1.0);
+}
+
+void renderGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
